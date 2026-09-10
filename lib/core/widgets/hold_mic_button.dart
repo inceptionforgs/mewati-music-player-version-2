17248e18ec1edbc95c7a07e5958cd1f47f21d9bc
Proton-email <experimentforpii@proton.me>
2026-09-10 11:32:02 +0530

    Update hold_mic_button.dart

--- lib/core/widgets/hold_mic_button.dart
+++ lib/core/widgets/hold_mic_button.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HoldMicButton extends StatefulWidget {
-  static const holdFor = Duration(seconds: 2);
-
+class HoldMicButton extends StatelessWidget {
   final VoidCallback onArmed;
   final Color idleColor;
   final Color holdColor;
@@ -20,60 +18,17 @@ class HoldMicButton extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<HoldMicButton> createState() => _HoldMicButtonState();
-}
-
-class _HoldMicButtonState extends State<HoldMicButton>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _hold;
-
-  @override
-  void initState() {
-    super.initState();
-    _hold = AnimationController(vsync: this, duration: HoldMicButton.holdFor)
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          HapticFeedback.mediumImpact();
-          widget.onArmed();
-          _hold.reset();
-        }
-      });
-  }
-
-  @override
-  void dispose() {
-    _hold.dispose();
-    super.dispose();
-  }
-
-  void _cancel() {
-    if (_hold.isAnimating || _hold.value > 0) {
-      _hold.animateBack(0, duration: const Duration(milliseconds: 160));
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Semantics(
       button: true,
-      label: widget.semanticLabel,
+      label: semanticLabel,
       child: GestureDetector(
         behavior: HitTestBehavior.opaque,
-        onTapDown: (_) => _hold.forward(from: _hold.value),
-        onTapUp: (_) => _cancel(),
-        onTapCancel: _cancel,
-        child: AnimatedBuilder(
-          animation: _hold,
-          builder: (context, _) {
-            final progress = _hold.value;
-            final color = Color.lerp(
-              widget.idleColor,
-              widget.holdColor,
-              progress,
-            )!;
-            return widget.builder(color, progress);
-          },
-        ),
+        onTap: () {
+          HapticFeedback.lightImpact();
+          onArmed();
+        },
+        child: builder(idleColor, 0),
       ),
     );
   }
